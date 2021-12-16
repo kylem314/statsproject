422e45415b9651a4c1d30bf45278250a66ac184f
Charlie <trollol678534@gmail.com>
2021-12-15 22:45:16 -0800

    changing some frontend

--- src/main/java/com/nighthawk/intellijs/controllers/AKjava.java
+++ src/main/java/com/nighthawk/intellijs/controllers/AKjava.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class AKjava {
     @GetMapping("/akabout")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String charlie_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String AK_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
         return "akabout"; // returns HTML VIEW (greeting)
