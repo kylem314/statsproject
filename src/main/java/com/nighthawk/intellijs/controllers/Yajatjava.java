0f9db95e003ad06753fa0f447c0848f4e2c6285e
Yajat <yyajat@gmail.com>
2021-12-16 08:50:46 -0800

    updated about us pages

--- src/main/java/com/nighthawk/intellijs/controllers/Yajatjava.java
+++ src/main/java/com/nighthawk/intellijs/controllers/Yajatjava.java
@@ -19,52 +19,65 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
 @Controller
 class Yajatjava {
-    @GetMapping("/yajat_about")
-    public String weather(Model model) throws IOException, InterruptedException, ParseException {
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://air-quality.p.rapidapi.com/forecast/airquality?lat=32.732&lon=-117.196&hours=72"))
-                .header("x-rapidapi-host", "air-quality.p.rapidapi.com")
-                .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        Object obj = new JSONParser().parse(response.body());
-        JSONObject jo = (JSONObject) obj;
-
-        JSONArray data = (JSONArray) jo.get("data");
-        JSONObject jo2 = (JSONObject) data.get(0);
-
-        model.addAttribute("jo2", jo2);
-        model.addAttribute("datetime", (String) jo2.get("datetime"));
-        model.addAttribute("aqi", jo2.get("aqi").toString());
-
-        return "yajat_about";
-
-
-    }
-
-
-
-
-    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://air-quality.p.rapidapi.com/forecast/airquality?lat=32.732&lon=-117.196&hours=72"))
-                .header("x-rapidapi-host", "air-quality.p.rapidapi.com")
-                .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        Object obj = new JSONParser().parse(response.body());
-        JSONObject jo = (JSONObject) obj;
-
-        JSONArray data = (JSONArray) jo.get("data");
-        JSONObject jo2 = (JSONObject) data.get(0);
-
-
-        System.out.println((String) jo2.get("datetime"));
-        System.out.println(jo2.get("aqi").toString());
+    @GetMapping("/yajatabout")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String yajat_about(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name); // MODEL is passed to html
+        return "yajatabout"; // returns HTML VIEW (greeting)
     }
 }
+
+
+
+//@Controller
+//class Yajatjava {
+//    @GetMapping("/yajat_about")
+//    public String weather(Model model) throws IOException, InterruptedException, ParseException {
+//
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("https://air-quality.p.rapidapi.com/forecast/airquality?lat=32.732&lon=-117.196&hours=72"))
+//                .header("x-rapidapi-host", "air-quality.p.rapidapi.com")
+//                .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
+//                .method("GET", HttpRequest.BodyPublishers.noBody())
+//                .build();
+//        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//        Object obj = new JSONParser().parse(response.body());
+//        JSONObject jo = (JSONObject) obj;
+//
+//        JSONArray data = (JSONArray) jo.get("data");
+//        JSONObject jo2 = (JSONObject) data.get(0);
+//
+//        model.addAttribute("jo2", jo2);
+//        model.addAttribute("datetime", (String) jo2.get("datetime"));
+//        model.addAttribute("aqi", jo2.get("aqi").toString());
+//
+//        return "yajat_about";
+//
+//
+//    }
+//
+//
+//
+//
+//    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("https://air-quality.p.rapidapi.com/forecast/airquality?lat=32.732&lon=-117.196&hours=72"))
+//                .header("x-rapidapi-host", "air-quality.p.rapidapi.com")
+//                .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
+//                .method("GET", HttpRequest.BodyPublishers.noBody())
+//                .build();
+//        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//        Object obj = new JSONParser().parse(response.body());
+//        JSONObject jo = (JSONObject) obj;
+//
+//        JSONArray data = (JSONArray) jo.get("data");
+//        JSONObject jo2 = (JSONObject) data.get(0);
+//
+//
+//        System.out.println((String) jo2.get("datetime"));
+//        System.out.println(jo2.get("aqi").toString());
+//    }
