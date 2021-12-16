04a78e863572376458e43aa96d0d5d47b6d98c50
Akshit Prathipati <54961927+akprathipati@users.noreply.github.com>
2021-12-15 18:30:42 -0800

    new stuff

--- src/main/java/com/nighthawk/intellijs/controllers/AKjava.java
+++ src/main/java/com/nighthawk/intellijs/controllers/AKjava.java
@@ -1,4 +1,15 @@
 package com.nighthawk.intellijs.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class AKjava {
+    @GetMapping("/akabout")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String charlie_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name); // MODEL is passed to html
+        return "akabout"; // returns HTML VIEW (greeting)
+    }
 }
+
