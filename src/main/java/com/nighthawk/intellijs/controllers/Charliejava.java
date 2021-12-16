2cda424d4308f66bed1e26d991cf544e91f2d415
Charlie <trollol678534@gmail.com>
2021-12-15 23:53:18 -0800

    added example of homepage and /

--- src/main/java/com/nighthawk/intellijs/controllers/Charliejava.java
+++ src/main/java/com/nighthawk/intellijs/controllers/Charliejava.java
@@ -29,4 +29,10 @@ public class Charliejava {
         model.addAttribute("name", name); // MODEL is passed to html
         return "charlieabout"; // returns HTML VIEW (greeting)
     }
+    @GetMapping("/home")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name); // MODEL is passed to html
+        return "home"; // returns HTML VIEW (greeting)
+    }
 }
