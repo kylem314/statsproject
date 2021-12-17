92f0e610de6b8b274de09cd751778c44bc6b1b67
Charlie <trollol678534@gmail.com>
2021-12-16 22:12:27 -0800

    trying to fix routing issue progress

--- src/main/java/com/nighthawk/intellijs/controllers/charliejava.java
+++ src/main/java/com/nighthawk/intellijs/controllers/charliejava.java
@@ -1,20 +1,27 @@
 package com.nighthawk.intellijs.controllers;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-public class charliejava {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class charliejava {
     @GetMapping("/charlieabout")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String charlie_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
         return "charlieabout"; // returns HTML VIEW (greeting)
-    }
-    @GetMapping("/home")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
-        model.addAttribute("name", name); // MODEL is passed to html
-        return "home"; // returns HTML VIEW (greeting)
     }
 }
