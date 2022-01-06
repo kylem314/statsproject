d23b5c2a9d196bdce765846cb46ab949ed881beb
Yajat <yyajat@gmail.com>
2022-01-06 08:49:51 -0800

    Worked on my about page and college database- Java and html

--- src/main/java/com/nighthawk/intellijs/controllers/Yajatjava.java
+++ src/main/java/com/nighthawk/intellijs/controllers/Yajatjava.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.io.IOException;
@@ -20,87 +21,132 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 
-//@Controller
-//public class Yajatjava {
-//    @GetMapping("/yajat_about")
-//
-//    public String yajat_about(Model model) {
-//
-//        String name = "test";
-//        model.addAttribute("name", name); // MODEL is passed to html
-//        return "yajat_about"; // returns HTML VIEW (greeting)
-//    }
-//}
-
-
 
 @Controller
 public class Yajatjava {
-    @GetMapping("/yajat_about")
-    public String weather(Model model) throws IOException, InterruptedException, ParseException {
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://weatherapi-com.p.rapidapi.com/ip.json?q=%3CREQUIRED%3E"))
-                .header("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
-                .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        Object obj = new JSONParser().parse(response.body());
-        JSONObject jo = (JSONObject) obj;
-
-        JSONObject current_data = (JSONObject) jo.get("current");
-
-
-
-//        String update = data.get("last_updated").toString();
-
-
-
-        model.addAttribute("jo", jo);
-        model.addAttribute("data", jo.get("current"));
-//        model.addAttribute("temp", current_data.get("temp_f") );
-//        model.addAttribute("wind_mph", current_data.get("wind_mph").toString() );
-//        model.addAttribute("humidity", current_data.get("humidity").toString() );
-//        model.addAttribute("last_updated", update );
-
-        return "yajat_about";
-
-
+    @GetMapping("/yajat_about")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String yajat_about(@RequestParam(name="name", required=false, defaultValue="x") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name); // MODEL is passed to html
+        return "yajat_about"; // returns HTML VIEW (greeting)
     }
 
+//        @GetMapping("/yajat_about")
+//        public String yajat_about(Model model) throws IOException, InterruptedException, ParseException {
 //
-//    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
-//        System.out.println(jo.get);
-
-
-
-
+//            HttpRequest request = HttpRequest.newBuilder()
+//                    .uri(URI.create("https://weatherapi-com.p.rapidapi.com/ip.json?q=%3CREQUIRED%3E"))
+//                    .header("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+//                    .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
+//                    .method("GET", HttpRequest.BodyPublishers.noBody())
+//                    .build();
+//            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//            Object obj = new JSONParser().parse(response.body());
+//            JSONObject jo = (JSONObject) obj;
+//
+//            JSONObject current_data = (JSONObject) jo.get("current");
+//
+//
+////        String update = data.get("last_updated").toString();
+//
+//
+//            model.addAttribute("jo", jo);
+//            model.addAttribute("data", jo.get("current"));
+//            model.addAttribute("temp", current_data.get("temp_f"));
+//            model.addAttribute("wind_mph", current_data.get("wind_mph").toString());
+//            model.addAttribute("humidity", current_data.get("humidity").toString());
+////        model.addAttribute("last_updated", current_data.get("update").toString());
+//
+//            return "yajat_about";
+//
+//
+//            //
+//////    FRQ Unit 4 Question 1
+////public String longestStreak(String str){
+////
+////    String previous = str.substring(0,1);
+////    String longest = "";
+////    String current = "";
+////
+////
+////    for (int x = 0; x < str.length(); x++){
+////
+////        if (str.substring(x, x + 1).equals(previous)){
+////
+////            current += str.substring(x, x+1);
+////            previous = str.substring(x, x+1);
+////
+////            if (current.length() > longest.length() ){
+////                longest = current;
+////            }
+////
+////
+////        }
+////
+////        else{
+////            current = "";
+////        }
+////
+////    }
+////
+////    return longest.substring(0,1) + " " + longest.length();
+////
+////}
+//        }
     }
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create("https://weatherapi-com.p.rapidapi.com/current.json?q=San%20Diego"))
-//                .header("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
-//                .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
-//                .method("GET", HttpRequest.BodyPublishers.noBody())
-//                .build();
-//        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-//        Object obj = new JSONParser().parse(response.body());
-//        JSONObject jo = (JSONObject) obj;
+
+////
+//        public static void main(String[]args) throws IOException, InterruptedException, ParseException {
+////        System.out.println(jo.get);
+//            HttpRequest request = HttpRequest.newBuilder()
+//                    .uri(URI.create("https://weatherapi-com.p.rapidapi.com/ip.json?q=%3CREQUIRED%3E"))
+//                    .header("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+//                    .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
+//                    .method("GET", HttpRequest.BodyPublishers.noBody())
+//                    .build();
+//            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//            Object obj = new JSONParser().parse(response.body());
+//            JSONObject jo = (JSONObject) obj;
 //
-////         JSONObject data = jo.get("current");
-////        JSONObject jo2 = (JSONObject) data.get(0);
+//            JSONObject current_data = (JSONObject) jo.get("current");
+//            System.out.println(current_data.get("temp_f"));
 //
-//        JSONObject data = (JSONObject) jo.get("current");
+////
+//////
+//////
+//////
+//////        HttpRequest request = HttpRequest.newBuilder()
+//////                .uri(URI.create("https://weatherapi-com.p.rapidapi.com/current.json?q=San%20Diego"))
+//////                .header("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+//////                .header("x-rapidapi-key", "bcdf06c872mshbd25dcebbd373b9p1afb3ajsncedd5c4986ed")
+//////                .method("GET", HttpRequest.BodyPublishers.noBody())
+//////                .build();
+//////        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//////        Object obj = new JSONParser().parse(response.body());
+//////        JSONObject jo = (JSONObject) obj;
+//////
+//////         JSONObject data = (JSONObject) jo.get("current");
+////// JSONObject jo2 = (JSONObject) data.get(0);
+//////        JSONObject data = (JSONObject) jo.get("current");
+//////
+//////        System.out.println(jo.get("current").toString());
+//////
+//////System.out.println(jo.get("current"));
+//////        System.out.println(data.get("temp_f")  );
+//////        System.out.println( data.get("wind_mph").toString() );
+//////        System.out.println( data.get("humidity").toString()  );
+//////        System.out.println( data.get("last_updated").toString()  );
+//////        System.out.println(jo2.get("aqi").toString());
+//////    }
 //
-//        System.out.println(jo.get("current").toString());
 //
-//System.out.println(jo.get("current"));
-//        System.out.println(data.get("temp_f")  );
-//        System.out.println( data.get("wind_mph").toString() );
-//        System.out.println( data.get("humidity").toString()  );
-//        System.out.println( data.get("last_updated").toString()  );
-////        System.out.println(jo2.get("aqi").toString());
-//    }
+//        }
+
+
+
+
+
 
 
 
