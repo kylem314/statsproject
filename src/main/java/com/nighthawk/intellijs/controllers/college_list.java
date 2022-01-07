d0416fcf5184b22313d0198b8f3e407d74d44ec3
ericl <ericliwang55555@gmail.com>
2022-01-07 09:02:16 -0800

    syntax fix

--- src/main/java/com/nighthawk/intellijs/controllers/college_list.java
+++ src/main/java/com/nighthawk/intellijs/controllers/college_list.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class collegelist {
+public class college_list {
     @GetMapping("/college_list")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String eric_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
