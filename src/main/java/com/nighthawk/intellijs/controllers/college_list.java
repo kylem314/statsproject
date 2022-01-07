4b725c4672ee83b97bb8fc9136bcc8fce5bfa4a6
ericl <ericliwang55555@gmail.com>
2022-01-07 08:50:48 -0800

    make college list

--- src/main/java/com/nighthawk/intellijs/controllers/college_list.java
+++ src/main/java/com/nighthawk/intellijs/controllers/college_list.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class college_list {
-    @GetMapping("/college_list")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    @GetMapping("/collegelist")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String eric_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
