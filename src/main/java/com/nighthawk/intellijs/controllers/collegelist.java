2c388cbebc0db6a19daaf8a049e47426b077b332
ericl <ericliwang55555@gmail.com>
2022-01-07 08:58:52 -0800

    college list fix

--- src/main/java/com/nighthawk/intellijs/controllers/collegelist.java
+++ src/main/java/com/nighthawk/intellijs/controllers/collegelist.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class college_list {
-    @GetMapping("/collegelist")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+public class collegelist {
+    @GetMapping("/college_list")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String eric_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
