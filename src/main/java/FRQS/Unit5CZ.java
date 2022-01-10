26d6f8e1f2b776edf8c33dd1b5c9d466c5ebff1e
Charlie <trollol678534@gmail.com>
2022-01-10 09:09:07 -0800

    FRQ 5 work

---
+++ src/main/java/FRQS/Unit5CZ.java
@@ -0,0 +1,26 @@
+package FRQS;
+
+public class Unit5CZ {
+    private String hostName;
+
+    private String address;
+
+
+
+    public Unit5CZ(String n, String a)
+
+    {
+
+        hostName = n;
+
+        address = a;
+
+    }
+    public String getHostName(){
+        return hostName;
+    }
+    public String updateAddress(String newAddress) {
+        String newaddress = newAddress;
+        return newaddress;
+    }
+}
